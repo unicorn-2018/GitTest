d45c2625a6f9cc3fe842d697a1efc4b34c2961c4
inoue <inoue@test>
2018-12-15 09:32:06 +0900

    20181215 ソース登録

---
+++ GitTest1/src/test/Main.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package test;
+
+/**
+ * @author Ino-UE
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO メッセージ出力 ユーザ毎に1行追加
+		// ※他ユーザのメッセージを上書きしないこと
+		System.out.printf("test1\n");
+		System.out.printf("s.inoue");
+
+	}
+
+}
